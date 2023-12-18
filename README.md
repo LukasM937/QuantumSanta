aac86280bad8095fce75da6d2fd250262ec653a3
Lukas Mührke <lukas.muehrke@tuhh.de>
2023-12-18 21:31:21 +0100

    Chore: - edit README - Add: CLI arguments

--- README.md
+++ README.md
@@ -3,16 +3,18 @@
 A small CLI app to distribute the roles for secret santa with true - quantum - randomness.
 Based on the IBM-Qiskit Framwork and use the noise of their quantum system to obtain random numbers.
 
-# A few disclaimers upfront:
+## A few disclaimers upfront:
 - This is not an example for efficent use of ressources - in fact it is the complet opposite of it.
 - In the current state the distributer - lets call them elfs - can see all the roles.
+### Usage:
+`./main.py [options]`
 
-# How to use the current State:
+## How to use the current State:
 - Edit the list in the script
 - Add the provider
 - Execute the script.
 
-# Roadmap:
+## Roadmap:
 - Add CLI commands
 - Remove hard coded list
 - Let the elfs participate:
--- src/main.py
+++ src/main.py
@@ -3,8 +3,12 @@ It runs the self checks as well as the sorting and order of particapents
 usage:
 ./main.py [options]'''
 
+import os
 import json
 import math
+import argparse
+import pathlib
+
 
 from test import *
 
@@ -28,81 +32,115 @@ class Participants:
         self.name = name
         self.receiver = receiver  # person_id to receive
 
-config_file = open('config.json')
-config_data = json.load(config_file)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-if config_data['Provider_Key'] != '':
-    provider_token = config_data['Provider_Key']
-else:
-    raise ValueError('No provider_token')
+    parser.add_argument('-t', '--test', action='store_true',
+                        help='The run is only for testing purposes.')
+    parser.add_argument('-vo','--validate_off', action='store_true',
+                        help='Disable the validation. I advise against it but I\'m not your mum')
+    parser.add_argument('-ll','--loop_lebel', choices=range(1,5), default=2,
+                        help='Specifies the min alowed length of loops')
+    parser.add_argument('-p', '-e', '--participants', '--elfs',
+                        help='A way to add participants to the script.')
+    parser.add_argument('-cp', '--config_path', type=pathlib.Path,
+                        default=os.getcwd()+'/config.json',
+                        help='Specify the relative parth form point of execution.')
+    parser.add_argument('-b','--backend'),
+    parser.add_argument('-k','--provider_key')
+    parser.add_argument('-v', '--verbose',
+                    action='store_false')  # on/off flag
+    args = parser.parse_args()
 
-provider = IBMProvider(token=provider_token)
+    if not args.backend:
+        args.test = True
 
-num_participants = len(config_data['Participants'])
-num_qbits = math.ceil(math.log2(num_participants**2))
+    if args.test:
+        backend = 'ibmq_qasm_simulator'
 
-qc = QuantumCircuit(num_qbits, num_qbits)
-for i in range(num_qbits):
-    qc.h(i)
+    print(args.test, args.backend)
 
-qc.measure(list(range(num_qbits)), list(range(num_qbits)))
+    try:
+        config_file = open(args.config_path)
+    except RuntimeError:
+        print('nothing here yet') #TODO: Add method to ask for config entries.
+    
+    config_data = json.load(config_file)
 
-#TODO: Add CLI cmd to change this - simulator == -t / --test
-backend = provider.get_backend('ibmq_qasm_simulator')
-#TODO: Add CLI cmd to change this - also to split it up, to make retries less costly.
-shots = 512
-
-transpiled_circuit = transpile(qc, backend)
-
-# Run the circuit and get result
-print(f'start job on: {backend}')
-job = backend.run(transpiled_circuit, shots=shots, memory=True)
-result = job.result()
-memory = result.get_memory()
-print(f'job finished on {backend}')
-
-part_list = []
-for i in config_data['Participants']:
-    part_list.append(Participants(i, i['name'], None))
-
-outputArray = []
-# convert the result to decimal
-for x in range(0, shots):
-    converted = int(memory[x], 2)
-    outputArray.append(converted%len(part_list))
-
-order = []
-i = 0
-while len(order) < len(part_list):
-    create_order(order, outputArray[0])
-    outputArray.pop(0)
-    i +=1
-
-orderd_list = [part_list[i] for i in order]
-
-# Calculation:
-for i in range(len(part_list)):
-    j = outputArray[0]
-    print(test_other_num_still_pressent(orderd_list[i].id, part_list, outputArray))
-    if test_other_num_still_pressent(orderd_list[i].id, part_list, outputArray):
-        while outputArray[j] == orderd_list[i].id:
-            j +=1
-            j = j%len(outputArray)
-        temp = outputArray[j]
-        orderd_list[i].receiver = part_list[temp]
-        print(f'{orderd_list[i].name}| {orderd_list[i].id} sends something to: {orderd_list[i].receiver.name} | {outputArray[j]}')
-        outputArray = list(filter((temp).__ne__, outputArray))
+    if config_data['Provider_Key'] != '':
+        provider_token = config_data['Provider_Key']
     else:
-        raise Exception('We encounted an issue during generation, restart script. - may the randomness be with you')
+        if args.provider_key is not None:
+            provider_token = args.provider_key
+        else:
+            raise ValueError('No provider_token, configure config.json or use the CLI argument.')
 
-# Tests:
-print(f'all Participants have some one: {test_all_give_someone(part_list)}')
+    provider = IBMProvider(token=provider_token)
 
-print(f'no self loops: {test_no_self_loops(part_list)}')
+    num_participants = len(config_data['Participants'])
+    num_qbits = math.ceil(math.log2(num_participants**2))
 
-print(f'no second order loops: {test_no_second_order_loops(part_list)}')
+    qc = QuantumCircuit(num_qbits, num_qbits)
+    for i in range(num_qbits):
+        qc.h(i)
 
-# Output:
-print('the result is:')
-for p in part_list:
-    print(f'{p.name.ljust(15)}, {p.receiver.name}')
+    qc.measure(list(range(num_qbits)), list(range(num_qbits)))
+
+    #TODO: Add CLI cmd to change this - simulator == -t / --test
+    backend = provider.get_backend('ibmq_qasm_simulator')
+    #TODO: Add CLI cmd to change this - also to split it up, to make retries less costly.
+    shots = 512
+
+    transpiled_circuit = transpile(qc, backend)
+
+    # Run the circuit and get result
+    print(f'start job on: {backend}')
+    job = backend.run(transpiled_circuit, shots=shots, memory=True)
+    result = job.result()
+    memory = result.get_memory()
+    print(f'job finished on {backend}')
+
+    part_list = []
+    for i in config_data['Participants']:
+        part_list.append(Participants(i, i['name'], None))
+
+    outputArray = []
+    # convert the result to decimal
+    for x in range(0, shots):
+        converted = int(memory[x], 2)
+        outputArray.append(converted%len(part_list))
+
+    order = []
+    i = 0
+    while len(order) < len(part_list):
+        create_order(order, outputArray[0])
+        outputArray.pop(0)
+        i +=1
+
+    orderd_list = [part_list[i] for i in order]
+
+    # Calculation:
+    for i in range(len(part_list)):
+        j = outputArray[0]
+        if test_other_num_still_pressent(orderd_list[i].id, part_list, outputArray):
+            while outputArray[j] == orderd_list[i].id:
+                j += 1
+                j = j%len(outputArray)
+            temp = outputArray[j]
+            orderd_list[i].receiver = part_list[temp]
+            outputArray = list(filter((temp).__ne__, outputArray))
+        else:
+            raise Exception('''We encounted an issue during generation, restart script.
+                            - may the randomness be with you''')
+
+    # Tests:
+    print(f'all Participants have some one: {test_all_give_someone(part_list)}')
+
+    print(f'no self loops: {test_no_self_loops(part_list)}')
+
+    print(f'no second order loops: {test_no_second_order_loops(part_list)}')
+
+    # Output:
+    print('the result is:')
+    for p in part_list:
+        print(f'{p.name.ljust(15)}, {p.receiver.name}')
